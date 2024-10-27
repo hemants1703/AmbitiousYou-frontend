5ff3664d06376d054cad1b458e092fa6f6c704a5
Hemant Sharma <itshemantsharma173@gmail.com>
2024-10-27 23:03:06 +0530

    added vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,10 @@
 	import { Toasts } from 'svoast';
 	import '../app.css';
 	import { ModeWatcher } from 'mode-watcher';
+	import { inject } from '@vercel/analytics';
+
+	// Injecting the analytics script from Vercel
+	inject();
 </script>
 
 <ModeWatcher />
